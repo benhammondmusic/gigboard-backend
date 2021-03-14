3dd26cc8a28d56be71ec7e0edea17a7b9d4ecec6
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-03-14 15:50:20 -0600

    change default local backend port to 5000 so nodemon and heroku local web both try for same port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const { user } = require('./Controllers');
 
 /* Port */
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 /* App */
 const app = express();
