c2d2281b9a4cdae107b263d5bf51c00bfb475b38
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-03-13 23:06:18 -0700

    comment out db access for now

--- config/database.js
+++ config/database.js
@@ -1,16 +1,16 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
+// require('dotenv').config();
+// const mongoose = require('mongoose');
 
-// CONNECT TO REMOTE MONGODB ATLAS
-mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+// // CONNECT TO REMOTE MONGODB ATLAS
+// mongoose.connect(process.env.DATABASE_URL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+// });
 
-// shortcut to mongoose.connection object
-const db = mongoose.connection;
+// // shortcut to mongoose.connection object
+// const db = mongoose.connection;
 
-db.on('connected', function () {
-  console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
-});
+// db.on('connected', function () {
+//   console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+// });
