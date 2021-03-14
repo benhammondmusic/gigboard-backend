38a93039118b5fa3d6be8366a18e45a5cca5f07f
Sergio Paiva Luna <sepaivaluna@sergios-mbp.home>
2021-03-13 19:43:18 -0500

    updated User and Post models

--- gigboard-Api/Models/User.js
+++ gigboard-Api/Models/User.js
@@ -6,6 +6,28 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  Posts: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
 });
 
 const User = mongoose.model("User", userSchema);
