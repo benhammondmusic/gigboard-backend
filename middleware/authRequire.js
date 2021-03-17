3e42e3ec23e67f68c6677b0ec18d015e3744b2aa
Sergio Paiva Luna <sepaivaluna@sergios-mbp.home>
2021-03-17 00:21:21 -0400

    added middleware jwt

---
+++ middleware/authRequire.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+
+const authRequired = (req, res, next) => {
+  try {
+    const bearerHeader = req.headers["authorization"];
+
+    if (typeof bearerHeader === "undefined") throw "forbidden";
+
+    const token = bearerHeader.split(" ")[1];
+
+    let payload = jwt.verify(token, process.env.SUPER_SECRET_KEY);
+
+    req.user = payload._id; // set user id for routes to use
+
+    next();
+  } catch (error) {
+    if (error === "forbidden") {
+      res.status(403).json({
+        status: 403,
+        message: "forbidden",
+      });
+    } else {
+      res.status(401).json({
+        status: 401,
+        ...error,
+      });
+    }
+  }
+};
+
+export default authRequired;
