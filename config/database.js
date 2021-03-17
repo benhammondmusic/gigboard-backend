dac3b670d50ef247d4cb33ced13d496fe1ae1b87
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-03-17 16:21:16 -0600

    comment

--- config/database.js
+++ config/database.js
@@ -14,3 +14,14 @@ const db = mongoose.connection;
 db.on('connected', function () {
   console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
 });
+
+/* FROM MONGODB ATLAS SETUP INSTRUCTIONS
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://gigboard-admin:taco@gigboard-db.gqkwa.mongodb.net/gigboard-data?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+}); */
