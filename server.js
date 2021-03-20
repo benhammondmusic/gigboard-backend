02f132b66c1d34a0d89dac7d6dc4f257229ed4f8
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-03-20 12:12:08 -0600

    register working, login isnt

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 dotenv.config();
 
 /* External Modules */
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
 /* Internal Modules */
-const { user } = require("./Controllers");
-require("./config/database");
-const routes = require("./routes");
+const { user } = require('./Controllers');
+require('./config/database');
+const routes = require('./routes');
 
 /* Port */
 const PORT = process.env.PORT || 5000;
@@ -19,24 +19,11 @@ const app = express();
 /* middleware */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// REMOVED WHITELIST - ALLOW ALL FOR NOW
 app.use(cors());
 
 /* Routes */
-app.get("/", (req, res) => {
-  res.send("Gigboard Backend API");
-});
-
-/* 
-// ROUTES
-app.use('/', routes.index); // contains OAUTH routes
-app.use('/places', routes.places); // ALL PLACE ROUTES, ADD REPORT CARD
-app.use('/reportcards', routes.reportcards); // VIEW ALL, DELETE, EDIT REPORT CARDS */
-
-// ROUTES
-app.use("/api/gigs", routes.gigs);
-app.use("/users", routes.users); // ALL USER ROUTES
+app.use('/api/gigs', routes.gigs); // ALL GIG ROUTES: CRUD
+app.use('/users', routes.users); // ALL USER ROUTES: login(), register()
 
 // app listening
 app.listen(PORT, () => console.log(`listening at port ${PORT}\n`));
