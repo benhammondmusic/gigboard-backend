610f9dc7c931799a313c7ae709bbe1670b502b86
Sergio Paiva Luna <sepaivaluna@sergios-mbp.home>
2021-03-20 13:26:40 -0400

    updated routes for jwt

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 /* External Modules */
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 /* Internal Modules */
-const { user } = require('./Controllers');
-require('./config/database');
-const routes = require('./routes');
+const { user } = require("./Controllers");
+require("./config/database");
+const routes = require("./routes");
 
 /* Port */
 const PORT = process.env.PORT || 5000;
@@ -24,19 +24,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 /* Routes */
-app.get('/', (req, res) => {
-  res.send('Gigboard Backend API');
+app.get("/", (req, res) => {
+  res.send("Gigboard Backend API");
 });
 
 /* 
 // ROUTES
 app.use('/', routes.index); // contains OAUTH routes
-app.use('/users', routes.users); // ALL USER ROUTES
 app.use('/places', routes.places); // ALL PLACE ROUTES, ADD REPORT CARD
 app.use('/reportcards', routes.reportcards); // VIEW ALL, DELETE, EDIT REPORT CARDS */
 
 // ROUTES
-app.use('/api/gigs', routes.gigs);
+app.use("/api/gigs", routes.gigs);
+app.use("/users", routes.users); // ALL USER ROUTES
 
 // app listening
 app.listen(PORT, () => console.log(`listening at port ${PORT}\n`));
