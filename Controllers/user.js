50c4dc5e3e1bb238f6008842051a24870b020088
Sergio Paiva Luna <sepaivaluna@sergios-mbp.home>
2021-03-23 21:49:36 -0400

    jwt sending to the frontend

--- Controllers/user.js
+++ Controllers/user.js
@@ -1,12 +1,12 @@
 // const { newUser } = require('../Models/Users/queries');
-const { User } = require('../Models');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
+const { User } = require("../Models");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 
 /* NOTE Register functionality */
 const register = async (req, res) => {
-  console.log(req.body, 'req.body inside user register');
+  console.log(req.body, "req.body inside user register");
   try {
     const { email } = req.body;
 
@@ -14,12 +14,12 @@ const register = async (req, res) => {
     let { password } = req.body;
 
     const foundUserResponse = await User.findOne({ email });
-    console.log(foundUserResponse, 'found user response');
+    console.log(foundUserResponse, "found user response");
 
     if (foundUserResponse) {
       return res.status(400).json({
         status: 400,
-        message: 'Email address already exists. Please try logging in instead of registering',
+        message: "Email address already exists. Please try logging in instead of registering",
       });
     }
 
@@ -35,19 +35,19 @@ const register = async (req, res) => {
     console.log(newUserPayload);
 
     const createUserResponse = await User.create(newUserPayload);
-    console.log('response from User.create()', createUserResponse);
+    console.log("response from User.create()", createUserResponse);
 
     res.status(201).json({
       currentUserId: createUserResponse._id,
       status: 201,
-      message: 'User created successfully',
+      message: "User created successfully",
       requestedAt: new Date().toLocaleDateString(),
     });
   } catch (error) {
-    console.log(error, 'ERROR REGISTERING USER');
+    console.log(error, "ERROR REGISTERING USER");
     return res.status(400).json({
       status: 400,
-      message: 'Something went wrong! Please try again ya fool',
+      message: "Something went wrong! Please try again ya fool",
     });
   }
 };
@@ -55,68 +55,75 @@ const register = async (req, res) => {
 /* NOTE Google Register */
 
 const registerGoogleUser = async (req, res) => {
-  console.log(req.body, 'req.body inside google register');
+  console.log(req.body, "req.body inside google register");
   try {
     const { email } = req.body;
 
     const foundUserResponse = await User.findOne({ email });
-    console.log(foundUserResponse, 'found user response');
+    console.log(foundUserResponse, "found user response");
 
     if (foundUserResponse) {
       return res.status(200).json({
         currentUserId: foundUserResponse._id,
         status: 200,
-        message: 'Found a user',
+        message: "Found a user",
       });
     }
 
     const createUserResponse = await User.create({ email });
-    console.log('response from User.create() VIA GOOGLE OAUTH', createUserResponse);
+    console.log("response from User.create() VIA GOOGLE OAUTH", createUserResponse);
 
     res.status(201).json({
       currentUserId: createUserResponse._id,
       status: 201,
-      message: 'User created successfully',
+      message: "User created successfully",
       requestedAt: new Date().toLocaleDateString(),
     });
   } catch (error) {
-    console.log(error, 'ERROR REGISTERING USER VIA GOOGLE OAUTH');
+    console.log(error, "ERROR REGISTERING USER VIA GOOGLE OAUTH");
     return res.status(400).json({
       status: 400,
-      message: 'Something went wrong! Please try again ya fool',
+      message: "Something went wrong! Please try again ya fool",
     });
   }
 };
 
 /* NOTE Login functionality */
 const login = async (req, res) => {
-  console.log(req.body, 'req.body inside user.login()');
+  console.log(req.body, "req.body inside user.login()");
   try {
     const { email, password } = req.body;
 
     /* This will check the user inputs both the email and password fields. If one is not filled then it will throw an error */
-    if (email === '' || password === '') {
-      console.log('Missing email and/or password');
-      throw 'missingInformation';
+    if (email === "" || password === "") {
+      console.log("Missing email and/or password");
+      throw "missingInformation";
     }
 
     /* Checking our database for the email entered on login page */
     const foundUserResponse = await User.findOne({ email });
 
     /* Throws error if couldn't find a user */
-    if (!foundUserResponse) throw 'invalidUser';
+    if (!foundUserResponse) throw "invalidUser";
+
+    const userId = foundUserResponse._id;
 
     /* Saving into a variable if the password entered is equal to the password of that user */
-    const match = await bcrypt.compare(password, foundUserResponse.password);
+    const isMatch = await bcrypt.compare(password, foundUserResponse.password);
 
-    if (match) {
-      console.log(foundUserResponse.password, 'found user password');
+    if (isMatch) {
+      console.log(foundUserResponse.password, "found user password");
+
+      const token = jwt.sign({ userId }, process.env.SUPER_SECRET_KEY, {
+        expiresIn: "24h",
+      });
 
       return res.status(200).json({
-        currentUserId: foundUserResponse._id,
+        auth: true,
+        token,
+        currentUserId: userId,
         status: 200,
-        message: 'Success',
-        // signedJwt,
+        message: "Success",
       });
     }
 
@@ -142,14 +149,14 @@ const login = async (req, res) => {
     // signedJwt,
     // });
   } catch (error) {
-    console.log(error, 'ERROR IN USER CTRL LOGIN()');
-    if (error === 'missingInformation') {
+    console.log(error, "ERROR IN USER CTRL LOGIN()");
+    if (error === "missingInformation") {
       return res.status(400).json({
         status: 400,
-        message: 'Email and password cannot be empty',
+        message: "Email and password cannot be empty",
       });
     }
-    if (error === 'invalidUser') {
+    if (error === "invalidUser") {
       return res.status(400).json({
         status: 400,
         message: "User doesn't exist",
@@ -158,7 +165,7 @@ const login = async (req, res) => {
 
     return res.status(500).json({
       status: 500,
-      message: 'Something went wrong. Please try again',
+      message: "Something went wrong. Please try again",
     });
   }
 };
